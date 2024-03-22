ccbddc69aa1aad69b21828ef038bd9650729c9b9
Ayhan <ayhan.mohammadi.dev@gmail.com>
2024-03-22 14:30:41 +0330

    Update base.php

--- test/base.php
+++ test/base.php
@@ -1,9 +1,13 @@
 <?php
+//V.2 
+// DAtaBase mysql
 
 error_reporting(0);
 include "telegram.php";
 
-$api = new Telegram("6179391015:");
+$api = new Telegram("6179391015:AAEEfndu7_wsl63AM-LG2Nb1YtVFbCN9EsU");
+$sql = new Database('server101_ayhan', 'go[j45@6ou}-lud@1u5o{', 'server101_ayhan');
+
 if (!is_file('URL.log')){ 
     $api->setHook('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
     file_put_contents('URL.log',"OK"); 
@@ -14,7 +18,11 @@ $from_id = $api->message()['from']['id'];
 $first_name = $api->message()['from']['first_name'];
 
 if($text === "/start"){
-    $api->sendMessage(array('chat_id' =>$from_id, 'text' =>"HI {$first_name} | @Ayhan_Dev"));
+       
+        $USER = $sql->exe_query("SELECT * FROM user WHERE id = '{$from_id}'")->fetch_assoc(); 
+        $from = $USER['from'];
+
+    $api->sendMessage(array('chat_id' =>$from_id, 'text' =>"HI {$first_name [$from]} | @Ayhan_Dev"));
 
     //Query DataBase
 }
